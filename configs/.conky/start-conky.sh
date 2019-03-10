aa1b6c4a8c4937a9d83c043373ffbe2c9b24d6eb
frontendmonster <frontendmonster@gmail.com>
2019-03-10 04:47:46 +0330

    conky: Add light and dark conky

--- configs/.conky/start-conky.sh
+++ configs/.conky/start-conky.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-conky -c "$HOME/.conky/date.pacsave"
-conky -c "$HOME/.conky/stats.pacsave"
+if [[ $* == *--dark* ]]; then
+  conky -c "$HOME/.conky/date.dark.pacsave"
+  conky -c "$HOME/.conky/stats.dark.pacsave"
+else
+  conky -c "$HOME/.conky/stats.light.pacsave"
+  conky -c "$HOME/.conky/date.light.pacsave"
+fi
