4bd2779ca5fe6b10c4cdb609ace1660086faf087
frontendmonster <frontendmonster@gmail.com>
2019-08-26 13:39:15 +0430

    (conky): Fix restart issue

--- configs/.conky/start-conky.sh
+++ configs/.conky/start-conky.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+killall conky
+
 if [[ $* == *--dark* ]]; then
-  conky -c "$HOME/.conky/date.dark.pacsave"
-  conky -c "$HOME/.conky/stats.dark.pacsave"
+  conky -c "$HOME/.conky/date.dark.pacsave" &
+  conky -c "$HOME/.conky/stats.dark.pacsave" &
 else
-  conky -c "$HOME/.conky/stats.light.pacsave"
-  conky -c "$HOME/.conky/date.light.pacsave"
+  conky -c "$HOME/.conky/stats.light.pacsave" &
+  conky -c "$HOME/.conky/date.light.pacsave" &
 fi
