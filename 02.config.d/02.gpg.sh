5d0be983460ee1e75bc0a5af0df660ec9b4fff0e
Alireza Safaierad <frontendmonster@gmail.com>
2021-08-24 13:43:57 +0400

    ask before creating gpg key

--- 02.config.d/02.gpg.sh
+++ 02.config.d/02.gpg.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 . "$(dirname "$BASH_SOURCE")/../utils.sh"
 
-title "Generating GPG Key..."
-gpg --full-gen-key
+mkdir -m 700 "${XDG_DATA_HOME:-$HOME/.local/share}/gnupg"
 
-progress "GPG key successfully created"
+while true; do
+  echo -en "Do you want to generate GPG key? [y/N] "
+  read -rn1 -p "" confirm
+  case $confirm in
+  [yY][eE][sS] | [yY])
+    title "Generating GPG Key..."
+    gpg --full-gen-key
+    progress "GPG key successfully created"
+    break
+    ;;
+  [nN][oO] | [nN])
+    echo
+    break
+    ;;
+  *)
+    echo "Invalid input..."
+    ;;
+  esac
+done
