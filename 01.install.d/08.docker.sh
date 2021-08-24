5dd7589669c4f8aae36e2685fdf1a05b969fbd65
Alireza Safaierad <frontendmonster@gmail.com>
2021-08-24 13:41:50 +0400

    fix shellcheck errors

--- 01.install.d/08.docker.sh
+++ 01.install.d/08.docker.sh
@@ -13,12 +13,12 @@ DCP_VERSION="v0.6.4"
 DCP_CLIENT="pass"
 DCP_URL="https://github.com/docker/docker-credential-helpers/releases/download/${DCP_VERSION}/docker-credential-${DCP_CLIENT}-${DCP_VERSION}-amd64.tar.gz"
 
-pushd "$(mktemp -d)"
+pushd "$(mktemp -d)" || exit 1
 wget -O dcp.tar.gz $DCP_URL
 tar -xvf dcp.tar.gz
 sudo mv docker-credential-pass /usr/bin/
 sudo chmod +x /usr/bin/docker-credential-pass
-popd
+popd || exit 1
 
 progress "Docker Service is enabled"
 
