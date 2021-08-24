f7f55cf3854feaed443eeb1e921d23893f5fa0ce
Alireza Safaierad <frontendmonster@gmail.com>
2021-08-24 13:42:05 +0400

    remove nodejs if exists

--- 01.install.d/09.node.sh
+++ 01.install.d/09.node.sh
@@ -2,9 +2,9 @@
 . "$(dirname "$BASH_SOURCE")/../utils.sh"
 
 title "Installing Volta"
-
+sudo pacman -Rns nodejs npm
 export VOLTA_HOME="$XDG_CONFIG_HOME/volta"
 curl https://get.volta.sh | bash
-volta install node
+$VOLTA_HOME/bin/volta install node
 
 progress "Done!"
