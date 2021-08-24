ece165fe52b8942630438e56308a5ab1a278546c
Alireza Safaierad <frontendmonster@gmail.com>
2021-08-24 13:43:27 +0400

    add fallback to XDG address

--- 01.install.d/12.neovim.sh
+++ 01.install.d/12.neovim.sh
@@ -6,13 +6,14 @@ sudo pacman -Sq neovim --noconfirm
 progress "Done!"
 
 title "Installing VIM Plug"
-VIM_PLUG_DIR="$XDG_DATA_HOME/nvim/site/autoload/plug.vim"
+VIM_PLUG_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim"
 curl -fLo "$VIM_PLUG_DIR" --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 progress "Done!"
+mkdir -p "${$XDG_CONFIG_HOME:-$HOME/.config}/wakatime"
 
 title "Running Neovim Python install"
-if exists pip3; then
+if [ ! -x "$(command -v pip3)" ]; then
   warn "Command pip3 Not Found!"
 else
   pip3 install --user neovim
