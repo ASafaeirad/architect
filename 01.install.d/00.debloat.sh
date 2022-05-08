02928bc739ac5147cf39bac823ea059eb9fc4337
Alireza Safaierad <frontendmonster@gmail.com>
2022-05-08 13:03:34 +0400

    sort debloat packages

--- 01.install.d/00.debloat.sh
+++ 01.install.d/00.debloat.sh
@@ -3,35 +3,35 @@
 
 title "Remove extra software"
 remove=(
-  i3-lock
-  i3lock-color
-  epdfview
-  palemoon-bin
-  lightdm
-  lightdm-slick-greeter
-  pamac
-  pamac-gtk
-  bmenu
-  vi
-  vim
-  vulkan-radeon
   arandr
   b43-fwcutter
+  bmenu
   crda
   cronie
   dfc
+  epdfview
+  i3-help
+  i3-lock
+  i3-scrot
+  i3lock-color
   lib32-vulkan-radeon
+  lightdm
+  lightdm-slick-greeter
   maia-console
   manjaro-i3-settings
   manjaro-zsh-config
-  i3-help
-  i3-scrot
+  palemoon-bin
+  pamac
+  pamac-gtk
+  vi
+  vim
+  vulkan-radeon
 )
 
 for pkg in "${remove[@]}"; do
   pkg_name=$(echo "$pkg" | awk '{print $1}')
   if pacman -Qi "$pkg" &>/dev/null ; then
     progress "Removing $pkg_name"
-    sudo pacman -Rns "$pkg" --noconfirm
+    sudo pacman -Rns "$pkg" --noconfirm > /dev/null
   fi
 done
