cbb3423fdea1069804e1e3939693c898a0d302d0
Alireza Safaierad <frontendmonster@gmail.com>
2022-05-09 11:20:00 +0400

    add fonts

--- 01.install.d/06.fonts.sh
+++ 01.install.d/06.fonts.sh
@@ -17,6 +17,7 @@ fonts=(
   persian-fonts
   shabnam-fonts
   terminus-font
+  ttf-cascadia-code
   ttf-courier-prime
   ttf-droid
   ttf-font-awesome
@@ -35,5 +36,5 @@ fonts=(
 for font in "${fonts[@]}"; do
   font_name=$(echo "$font" | awk '{print $1}')
   progress "Installing $font_name"
-  paru -Sq "$font" --noconfirm > /dev/null
+  paru -Sq "$font" --noconfirm >/dev/null
 done
