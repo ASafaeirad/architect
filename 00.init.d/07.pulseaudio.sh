6d70609aeaf498164bb03312bb4b1ad5f2460fc6
Alireza Safaierad <frontendmonster@gmail.com>
2022-05-09 11:43:56 +0400

    add pulseaudio script

---
+++ 00.init.d/07.pulseaudio.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+. "$(dirname "$BASH_SOURCE")/../utils.sh"
+
+title "Installing xf86-input-libinput"
+sudo pacman -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth --noconfirm > /dev/null
+
+progress "Done!"
