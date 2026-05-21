b88540da88675ede33f0ac4c4cc9b7582e85df29
Alireza Safaierad <alireza.safaierad@gmail.com>
2026-05-22 00:22:27 +0200

    add bluetui

--- 00.init.d/06.bluetooth.sh
+++ 00.init.d/06.bluetooth.sh
@@ -2,6 +2,6 @@
 . "$(dirname "$BASH_SOURCE")/../utils.sh"
 
 title "Installing bluz"
-sudo pacman -S bluez bluez-libs bluez-utils --noconfirm > /dev/null
+sudo pacman -S bluez bluetui bluez-libs bluez-utils --noconfirm > /dev/null
 
 progress "Done!"
